89cbb167feb35a7c0b34d7c06581d7e3ff2abff6
Shubham Awasthi <shubham0522@gmail.com>
2018-06-15 12:25:31 +0530

    Added scripts

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from flask import Flask
-from datetime import datetime
+from flask import Flask, jsonify, Response, request
+from preproc_beta import preproc
+from TT_SLOT import slot
+import os
 app = Flask(__name__)
+#
+#
+# a={'a':'b'}
+# @app.route('/',methods=['post','get'])
+# def index():
+#     # resp = Response('{\"a\":"b"}')
+#     # resp.headers['content-type'] = 'application/json'
+#     # # return ("{\"as\":\"fg\"}", 400)
+#     # # return jsonify(a)
+#     res=Response("Hey")
+#     res.set_cookie("name", value="I am cookie")
+#     # return jsonify({'form':request.form,'args':request.args,'cookie':request.cookies})
+#     return res
+#
+# app.run(port=3000)
 
-@app.route('/')
-def homepage():
-    return "hey"
+# proc('C:\\Users\\shubh\\Desktop\\Shubham-IT2.png')
+UPLOAD_FOLDER = './tmp/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/',methods=['post','get'])
+def index():
+    return '<form action="/upload" method="post" enctype="multipart/form-data"><input type="file" name="file" /><input type="submit"></form>'
+
+
+@app.route('/upload',methods=['post','get'])
+def upload_file():
+    f = request.files['file']
+    f.save('./tmp/image.jpg')
+    preproc('./tmp/image.jpg')
+    return(slot())
+    return "as"
+
 
 if __name__ == '__main__':
-	app.run()
-
+    app.run(debug=True)
