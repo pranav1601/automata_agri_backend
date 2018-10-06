d4f36b4e2bea9bb33531545d83965ba260e1563c
Shubham Awasthi <aw.shubh@gmail.com>
2018-10-06 14:44:52 +0530

    Added keys file

--- addon/getCondition.py
+++ addon/getCondition.py
@@ -1,16 +1,16 @@
 import requests
 import json
-import key
-
+import keys
+# print(keys.keys.accuKey)
 def exec(lat,lng):
-    try:
-        url='http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey='+key.accuKey+'&q='+str(lat)+','+str(lng)
+    # try:
+        url='http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey='+keys.keys.accuKey+'&q='+str(lat)+','+str(lng)
         locKey=json.loads(requests.get(url).text)['Key']
-        url2='http://dataservice.accuweather.com/currentconditions/v1/'+locKey+'?apikey='+key.accuKey+'&details=true'
+        url2='http://dataservice.accuweather.com/currentconditions/v1/'+locKey+'?apikey='+keys.keys.accuKey+'&details=true'
         locDetais=json.loads(requests.get(url2).text)
         temp=locDetais[0]['Temperature']['Metric']['Value']
         humidity=locDetais[0]['RelativeHumidity']
         wind=locDetais[0]['Wind']['Speed']['Metric']['Value']
         return({'temp':temp,'humidity':humidity,'wind':wind})
-    except:
+    # except:
         return None
--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify, Response, request
 import sys, os
 sys.path.insert(0, './addon')
+sys.path.insert(0, './config')
 import getCondition
 app = Flask(__name__)
 
 @app.route('/',methods=['post','get'])
 def index():
-    return 'Server is running'
-print(getCondition.exec(12.975358300000002,79.1604862))
+    return jsonify(getCondition.exec(12.975358300000002,79.1604862))
+
+# print(getCondition.exec(12.975358300000002,79.1604862))
 
 debug=False
 if('HEROKU' not in os.environ):
