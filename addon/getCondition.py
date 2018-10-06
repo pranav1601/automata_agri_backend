e36f3c6322cd521a9220c3501dba50082db33b2d
Shubham Awasthi <aw.shubh@gmail.com>
2018-10-06 15:53:22 +0530

    Changed to apixu

--- addon/getCondition.py
+++ addon/getCondition.py
@@ -4,13 +4,14 @@ import keys
 # print(keys.keys.accuKey)
 def exec(lat,lng):
     try:
-        url='http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey='+keys.keys.accuKey+'&q='+str(lat)+','+str(lng)
-        locKey=json.loads(requests.get(url).text)['Key']
-        url2='http://dataservice.accuweather.com/currentconditions/v1/'+locKey+'?apikey='+keys.keys.accuKey+'&details=true'
-        locDetais=json.loads(requests.get(url2).text)
-        temp=locDetais[0]['Temperature']['Metric']['Value']
-        humidity=locDetais[0]['RelativeHumidity']
-        wind=locDetais[0]['Wind']['Speed']['Metric']['Value']
-        return({'temp':temp,'humidity':humidity,'wind':wind})
+        url='http://api.apixu.com/v1/forecast.json?key='+keys.keys.accuKey+'&q='+str(lat)+','+str(lng)+'&days=10'
+        locDetais=json.loads(requests.get(url).text)
+        print(locDetais)
+        prep=0
+        for i in locDetais["forecast"]["forecastday"]:
+            prep=prep+(i["day"]["totalprecip_mm"])
+        return prep
     except:
-        return None
+        return 0
+
+# exec(12.9745083,79.1572194)
