3348dc00b2391ae61fdf4bc317148120ea5b427e
Shubham Awasthi <aw.shubh@gmail.com>
2018-10-06 14:30:47 +0530

    Gets curent condition from accuweather

---
+++ addon/getCondition.py
@@ -0,0 +1,16 @@
+import requests
+import json
+import key
+
+def exec(lat,lng):
+    try:
+        url='http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey='+key.accuKey+'&q='+str(lat)+','+str(lng)
+        locKey=json.loads(requests.get(url).text)['Key']
+        url2='http://dataservice.accuweather.com/currentconditions/v1/'+locKey+'?apikey='+key.accuKey+'&details=true'
+        locDetais=json.loads(requests.get(url2).text)
+        temp=locDetais[0]['Temperature']['Metric']['Value']
+        humidity=locDetais[0]['RelativeHumidity']
+        wind=locDetais[0]['Wind']['Speed']['Metric']['Value']
+        return({'temp':temp,'humidity':humidity,'wind':wind})
+    except:
+        return None
--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, jsonify, Response, request
+import sys, os
+sys.path.insert(0, './addon')
+import getCondition
 app = Flask(__name__)
 
 @app.route('/',methods=['post','get'])
 def index():
     return 'Server is running'
+print(getCondition.exec(12.975358300000002,79.1604862))
+
+debug=False
+if('HEROKU' not in os.environ):
+    debug=True
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=debug)
